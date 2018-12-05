8c1e8eeae543b3c5ae92b38f63c465107e794499
Soma Lucz <soma.lucz@diplomatiq.org>
2018-12-05 16:23:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# project-config
+
+General project/repository settings and configuration for Diplomatiq projects.
+
+## Initializing a new project
+
+1. Create a new repository on GitHub under Diplomatiq. Add a **name** and some **description**. Set the access to **Public**. No README, no .gitignore, no license.
+2. `cd ~/projects/diplomatiq`
+3. `bash ./project-config/init-project.sh PROJECTNAME`
+
+---
+
+Copyright (c) 2018 Diplomatiq
---
+++ init-project.sh
@@ -0,0 +1,44 @@
+# Example usage e.g. in the ~/projects/diplomatiq directory: bash ./project-config/create-project.sh project-name
+# This will create a directory for the project in ~/project/diplomatiq and perform the necessary configuration there.
+
+projectUserName="Soma Lucz"
+projectUserEmail="soma.lucz@diplomatiq.org"
+projectUserSigningKey="11B962F8C64804A7"
+projectCommitGpgSign=true
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: bash init-project.sh projectname \"GitHub description\""
+    exit 1
+fi
+
+if ! [[ $1 =~ ^[a-zA-Z0-9_-]+$ ]]; then
+    echo "Error: URL-safe project name required"
+    exit 1
+fi
+
+directoryOfThisScript="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+projectName=$1
+description=$2
+
+mkdir $projectName
+pushd $projectName
+
+git init
+printf "# $projectName\n\n$description\n\n---\n\nCopyright (c) 2018 Diplomatiq\n" > README.md
+
+cp $directoryOfThisScript/LICENSE .
+cp $directoryOfThisScript/.editorconfig .
+cp -r $directoryOfThisScript/.github .
+
+git config --local user.name "$projectUserName"
+git config --local user.email $projectUserEmail
+git config --local user.signingkey $projectUserSigningKey
+git config --local commit.gpgsign $projectCommitGpgSign
+
+git remote add origin git@github.com:Diplomatiq/$projectName.git
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+
+popd
